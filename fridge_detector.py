bc1e5463d43de830919d29b6a1b2131b6b5c8db4
lmaksin <lmaksin@umich.edu>
2023-10-14 19:02:05 -0700

    fridge detector model (API call)

---
+++ fridge_detector.py
@@ -0,0 +1,61 @@
+
+##################################################################################################
+# In this section, we set the user authentication, user and app ID, model details, and the URL
+# of the image we want as an input. Change these strings to run your own example.
+#################################################################################################
+
+# Your PAT (Personal Access Token) can be found in the portal under Authentification
+PAT = '047995b35d044edaa2a1a58c437934e2'
+# Specify the correct user_id/app_id pairings
+# Since you're making inferences outside your app's scope
+USER_ID = 'clarifai'
+APP_ID = 'main'
+# Change these to whatever model and image URL you want to use
+MODEL_ID = 'food-item-recognition'
+MODEL_VERSION_ID = '1d5fd481e0cf4826aa72ec3ff049e044'
+
+############################################################################
+# YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+############################################################################
+
+from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+from clarifai_grpc.grpc.api.status import status_code_pb2
+
+def make_request(image_url):
+    channel = ClarifaiChannel.get_grpc_channel()
+    stub = service_pb2_grpc.V2Stub(channel)
+
+    metadata = (('authorization', 'Key ' + PAT),)
+
+    userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+
+    post_model_outputs_response = stub.PostModelOutputs(
+        service_pb2.PostModelOutputsRequest(
+            user_app_id=userDataObject,  # The userDataObject is created in the overview and is required when using a PAT
+            model_id=MODEL_ID,
+            version_id=MODEL_VERSION_ID,  # This is optional. Defaults to the latest model version
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(
+                        image=resources_pb2.Image(
+                            url=IMAGE_URL
+                        )
+                    )
+                )
+            ]
+        ),
+        metadata=metadata
+    )
+    if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+        print(post_model_outputs_response.status)
+        raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
+
+    # Since we have one input, one output will exist here
+    output = post_model_outputs_response.outputs[0]
+
+    predicted_foods = []
+    for concept in output.data.concepts:
+        predicted_foods.append(concept.name)
+    
+    return predicted_foods
